9e5bc55cbc382117636349a5d4ef81c6c1f7ad51
Rony <ronyjavierramirezalvarado@gmail.com>
2025-09-23 02:53:24 -0600

    Cambios en la navegacion

--- frontend/src/navigation/MainLayout.js
+++ frontend/src/navigation/MainLayout.js
@@ -7,12 +7,22 @@ import ProductsScreen from '../screens/Private/Products/Products.js';
 import EmployeesScreen from '../screens/Private/Employees/Employee.js';
 import AccesoriesScreen from '../screens/Public/AccesoriesScreen';
 import FestivitiesScreen from '../screens/Public/FestivitiesScreen';
+import LoginScreen from '../screens/Login/Login.js';
+import ChooseScreen from '../screens/Register/Choose';
+import RegisterScreen from '../screens/Register/Register';
+import RegisterVetScreen from '../screens/Register/RegisterVet';
+import VerificationCodeScreen from '../screens/Register/VerificationCode.js';
 
 const Stack = createNativeStackNavigator();
 
 export default function MainLayout() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="ChooseAccount" component={ChooseScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="RegisterVet" component={RegisterVetScreen} />
+      <Stack.Screen name="VerificationCode" component={VerificationCodeScreen} options={{ gestureEnabled: false, headerBackVisible: false }} />
       <Stack.Screen name="BottomTabs" component={MyTabs} />
       <Stack.Screen name="Bandanas" component={BandanasScreen} />
       <Stack.Screen name="Collares" component={CollarsScreen} />
