1861f4366e7ce6d901e8abc192c2c70df5bb6058
Rony <ronyjavierramirezalvarado@gmail.com>
2025-09-24 11:53:04 -0600

    Cambios en para la navegacion por roles - parte 1

--- frontend/src/navigation/MainLayout.js
+++ frontend/src/navigation/MainLayout.js
@@ -13,25 +13,56 @@ import RegisterScreen from '../screens/Register/Register';
 import RegisterVetScreen from '../screens/Register/RegisterVet';
 import VerificationCodeScreen from '../screens/Register/VerificationCode.js';
 import ClientsScreen from '../screens/Private/Clients/Clients.js';
-
+import { useAuth } from '../context/AuthContext';
+ 
+ 
 const Stack = createNativeStackNavigator();
-
+ 
 export default function MainLayout() {
+  const { user } = useAuth()
+
+  const getInitialRouter = () => {
+    if (!user) return 'Login';
+ 
+    switch (user.userType) {
+      case 'admin':
+      case 'employee':
+        return 'ProductsScreen';
+      case 'client':
+      case 'vet':
+        return 'BottomTabs';
+      default:
+        return 'Login';
+    }
+  }
+ 
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="ChooseAccount" component={ChooseScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen name="RegisterVet" component={RegisterVetScreen} />
-      <Stack.Screen name="VerificationCode" component={VerificationCodeScreen} options={{ gestureEnabled: false, headerBackVisible: false }} />
-      <Stack.Screen name="BottomTabs" component={MyTabs} />
-      <Stack.Screen name="Bandanas" component={BandanasScreen} />
-      <Stack.Screen name="Collares" component={CollarsScreen} />
-      <Stack.Screen name="Accesorios" component={AccesoriesScreen} />
-      <Stack.Screen name="FestivitiesScreen" component={FestivitiesScreen} />
-      <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
-      <Stack.Screen name="EmployeesScreen" component={EmployeesScreen} />
-      <Stack.Screen name="ClientsScreen" component={ClientsScreen} />
-    </Stack.Navigator>
+    <Stack.Navigator
+    initialRouteName={getInitialRouter()}
+    screenOptions={{ headerShown: false }}
+  >
+    {/* Pantallas de autenticación */}
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="ChooseAccount" component={ChooseScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen name="RegisterVet" component={RegisterVetScreen} />
+    <Stack.Screen
+      name="VerificationCode"
+      component={VerificationCodeScreen}
+      options={{ gestureEnabled: false, headerBackVisible: false }}
+    />
+
+    {/* Rutas públicas */}
+    <Stack.Screen name="BottomTabs" component={MyTabs} />
+    <Stack.Screen name="Bandanas" component={BandanasScreen} />
+    <Stack.Screen name="Collares" component={CollarsScreen} />
+    <Stack.Screen name="Accesorios" component={AccesoriesScreen} />
+    <Stack.Screen name="FestivitiesScreen" component={FestivitiesScreen} />
+
+    {/* Rutas privadas */}
+    <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
+    <Stack.Screen name="EmployeesScreen" component={EmployeesScreen} />
+    <Stack.Screen name="ClientsScreen" component={ClientsScreen} />
+  </Stack.Navigator>
   );
 }
